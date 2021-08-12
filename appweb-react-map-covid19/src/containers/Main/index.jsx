91b0c518e7d7a5073d0add517d3a902e6fc8d240
Ricardo <ricardoaraujo3713@gmail.com>
2021-08-12 15:18:33 -0300

    Criando novo arquivo api.js utilizando o path do browser para requisição da api com fech importando useState, useCallback, useEffect ao index.jsx, requisitando dados do brazil como teste

--- appweb-react-map-covid19/src/containers/Main/index.jsx
+++ appweb-react-map-covid19/src/containers/Main/index.jsx
@@ -1,6 +1,20 @@
-import React, { memo } from "react";
+import React, { memo, useState, useCallback, useEffect } from "react";
+import Api from '../../api'
 
 function Main(){
+    const [data, setData] = useState({})
+
+    const [country, setCountry] = useState('brazil')
+
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country)
+        .then(data => setData())
+    }, [])
+
+    useEffect(() => {
+        getCovidData(country)
+    }, [getCovidData, country]);
+
     return(
         <div>
             Teste
