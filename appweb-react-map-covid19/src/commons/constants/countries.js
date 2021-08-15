55126e6b3615dbf3315fee5d1142629d8dc7aca8
Ricardo <ricardoaraujo3713@gmail.com>
2021-08-15 13:55:55 -0300

    Inserindo dados dos paises da America do Sul

--- appweb-react-map-covid19/src/commons/constants/countries.js
+++ appweb-react-map-covid19/src/commons/constants/countries.js
@@ -3,8 +3,71 @@ import ChinaFlag from '../../assets/images/china.png'
 import UsaFlag from '../../assets/images/usa.png'
 import ItalyFlag from '../../assets/images/italy.png'
 import GermanyFlag from '../../assets/images/germany.png'
+import ArgentinaFlag from '../../assets/images/argentina.png'
+import BoliviaFlag from '../../assets/images/bolivia.png'
+import ChileFlag from '../../assets/images/chile.png'
+import ColombiaFlag from '../../assets/images/colombia.png'
+import EcuadorFlag from '../../assets/images/ecuador.png'
+import ParaguayFlag from '../../assets/images/paraguay.png'
+import PeruFlag from '../../assets/images/peru.png'
+import UruguayFlag from '../../assets/images/uruguay.png'
+import VenezuelaFlag from '../../assets/images/venezuela.png'
 
 const COUNTRIES = [
+    {
+        label: 'Venezuela',
+        value: 'venezuela',
+        flag: VenezuelaFlag
+    },
+
+    {
+        label: 'Uruguay',
+        value: 'uruguay',
+        flag: UruguayFlag
+    },
+
+    {
+        label: 'Peru',
+        value: 'peru',
+        flag: PeruFlag
+    },
+
+    {
+        label: 'Paraguay',
+        value: 'paraguay',
+        flag: ParaguayFlag
+    },
+
+    {
+        label: 'Ecuador',
+        value: 'ecuador',
+        flag: EcuadorFlag
+    },
+
+    {
+        label: 'Colombia',
+        value: 'colombia',
+        flag: ColombiaFlag
+    },
+
+    {
+        label: 'Chile',
+        value: 'chile',
+        flag: ChileFlag
+    },
+
+    {
+        label: 'Bolivia',
+        value: 'bolivia',
+        flag: BoliviaFlag
+    },
+
+    {
+        label: 'Argentina',
+        value: 'argentina',
+        flag: ArgentinaFlag
+    },
+
     {
         label: 'Germany',
         value: 'germany',
